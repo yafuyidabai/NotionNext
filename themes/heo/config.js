580eeeb0ba2c9d911645ea80c203ad9dc142f11b
yafuyidabai <79457833+yafuyidabai@users.noreply.github.com>
2024-01-08 08:47:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,17 +5,17 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '喜鹊喳喳叫', url: 'https://goldfishies.com' },
+    { title: '必有客人到', url: 'https://sharkle.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '吃喝玩乐',
+  HEO_HERO_TITLE_2: '再少学一点',
+  HEO_HERO_TITLE_3: 'Bailuobu.COM',
+  HEO_HERO_TITLE_4: '在线更新',
+  HEO_HERO_TITLE_5: '打开notion一键轻松复制',
+  HEO_HERO_TITLE_LINK: 'https://dour-zebu-cb5.notion.site/dd94928b247a4310bc0bf8823e9ee7a3?v=d7004e2693e1467c8a2c08f6d47d1f2a&pvs=74',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
